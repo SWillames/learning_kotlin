84f7cf5f11a4d599582293f123b660df3b693427
Sergio Costa <sergiowoc@gmail.com>
2023-11-20 18:16:10 -0300

    comments in kotlin learned.

---
+++ hello_world/src/main/kotlin/br/com/sw_software/comments/Main.kt
@@ -0,0 +1,20 @@
+package br.com.sw_software.comments
+
+fun main(){
+  // I'm a comment of end of line
+  var test = "Testing comments" // comment from here
+
+  /*
+    I am a comment too
+    I am a block comment
+    This is many lines commented
+   */
+}
+
+/*
+fun fooBar(){
+  /*
+    I am embedded comment
+   */
+}
+*/
