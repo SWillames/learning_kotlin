77be5a3537ece3a76e8f737d11ec70e5fab672a0
Sergio Costa <sergiowoc@gmail.com>
2023-11-20 12:10:57 -0300

    add args the first project.

--- hello_world/src/main/kotlin/br/com/sw_software/Main.kt
+++ hello_world/src/main/kotlin/br/com/sw_software/Main.kt
@@ -1,5 +1,10 @@
 package br.com.sw_software
 
-fun main() {
-  println("Hello, world!!")
+//fun main() {
+//  println("Hello, world!!")
+//}
+
+fun main(args: Array<String>){
+  val name = if (args.isNotEmpty()) args[0] else "Kotlin World"
+  println("Hello, $name!!!")
 }
